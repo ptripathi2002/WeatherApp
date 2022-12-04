a4d6e692f45d9f09fec342be68f58ec8dcba3aaa
Prashant Tripathi <itsmeprashant02@gmail.com>
2022-12-05 00:42:02 +0530

    final Commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "db8c00c844msh7a359b3146ef934p155048jsn77897bbb7ae7",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "94d490883cf03d5230c2d35f4bc4bf8a"; 
